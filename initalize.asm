e6b649286a056fb7b7fac90387d20e15a6a71145
Jack Helmer <rjacksonhelmer@gmail.com>
2022-05-26 21:35:18 -0400

    Fixing over-written file

--- initalize.asm
+++ initalize.asm
@@ -1,31 +1,12 @@
 section .text
 	global main
 main:
-	mov edx,1
-	mov ecx,choice
-	mov ebx,1
-	mov eax,4
-	int 0x80
-
-	mov eax,1
-	int 0x80
-
-section .data
-choice DB 'Y'
-choice DB 'Y'
-number1 DW 12345
-number2 DD 12345679
-marks TIMES 9 DW 0
-
-section .text
-	global main
-main:
 	mov edx,9
 	mov ecx,stars
 	mov ebx,1
 	mov eax,4
 	int 0x80
-	
+
 	mov eax,1
 	int 0x80
 
