68c910fb260568d955b9c982ca6abe573f070cf5
C0uchpotato <rjacksonhelmer@gmail.com>
2022-06-06 22:25:37 -0400

    Add & Sub + notes

---
+++ Increment and Decrement/Inc_and_Dec.asm
@@ -0,0 +1,73 @@
+segment .Data
+
+        msg1 db "Enter a digit", 0xA, 0xD
+        len1 equ $ - msg1
+
+        msg2 db "Please enter a second digit", 0xA, 0xD
+        len2 equ $ - msg2
+
+        msg3 db "The sum is: "
+        len3 equ $ - msg3
+
+segment .bss
+
+num1 resb 2
+num2 resb 2
+res resb 1
+
+section .text
+        global main
+main:
+        mov eax, 4
+        mov ebx, 1
+        mov ecx, msg1
+        mov edx, len1
+        int 0x80
+
+        mov eax, 3
+        mov ebx, 0
+        mov ecx, num1
+        mov edx, 2
+        int 0x80
+
+        mov eax, 3
+        mov ebx, 1
+        mov ecx, msg2
+        mov edx, len2
+        int 0x80
+
+        mov eax, 3
+        mov ebx, 0
+        mov ecx, num2
+        mov edx, 2
+        int 0x80
+
+        mov eax, 3
+        mov ebx, 1
+        mov ecx, msg3
+        mov edx, len3
+        int 0x80
+
+        ;moving the first number to the eax register and the second number to the EBX
+        ;and subtracting asciii '0' to convert it into a decimal numbers
+        mov eax, num1
+        sub eax, '0'
+        mov ebx, num2
+        sub ebx, '0'
+
+        ;add ebx and EAX
+
+        add eax, EBX
+        add eax, 0
+
+        mov res, EAX
+
+        mov eax, 4
+        mov ebx, 1
+        mov ecx, res
+        mov edx,1
+        int 0x80
+exit:
+        mov eax,1
+        xor ebx, ebx
+        int 0x80
