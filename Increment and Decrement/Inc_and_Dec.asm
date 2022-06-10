c3d57b40edf099c84c29de50a3c2c089e4b2d929
C0uchpotato <rjacksonhelmer@gmail.com>
2022-06-09 21:25:23 -0400

    Refreshing diffs

--- Increment and Decrement/Inc_and_Dec.asm
+++ Increment and Decrement/Inc_and_Dec.asm
@@ -1,4 +1,11 @@
-segment .Data
+SYS_EXIT equ 1
+SYS_READ equ 3
+SYS_WRITE equ 4
+STDIN equ 0
+STDOUT equ 1
+
+
+segment .data
 
         msg1 db "Enter a digit", 0xA, 0xD
         len1 equ $ - msg1
@@ -12,38 +19,38 @@ segment .Data
 segment .bss
 
         num1 resb 2
-        res resb 1
         num2 resb 2
+        res resb 1
 
 section .text
         global main     ;must be declared for linker
 main:           ;tell linker entry point
-        mov eax, 4
-        mov ebx, 1
+        mov eax, SYS_WRITE
+        mov ebx, STDOUT
         mov ecx, msg1
         mov edx, len1
         int 0x80
 
-        mov eax, 3
-        mov ebx, 0
+        mov eax, SYS_READ
+        mov ebx, STDIN
         mov ecx, num1
         mov edx, 2
         int 0x80
 
-        mov eax, 4
-        mov ebx, 1
+        mov eax, SYS_WRITE
+        mov ebx, STDOUT
         mov ecx, msg2
         mov edx, len2
         int 0x80
 
-        mov eax, 3
-        mov ebx, 0
+        mov eax, SYS_READ
+        mov ebx, STDIN
         mov ecx, num2
         mov edx, 2
         int 0x80
 
-        mov eax, 4
-        mov ebx, 1
+        mov eax, SYS_WRITE
+        mov ebx, STDOUT
         mov ecx, msg3
         mov edx, len3
         int 0x80
@@ -58,16 +65,16 @@ main:           ;tell linker entry point
         ;add ebx and EAX
 
         add eax, ebx
-        add eax, 0
+        add eax, '0'
 
-        mov edx, eax
+        mov [res], eax
 
-        mov eax, 4
-        mov ebx, 1
-        mov ecx, edx
+        mov eax, SYS_WRITE
+        mov ebx, STDOUT
+        mov ecx, res
         mov edx, 1
         int 0x80
 exit:
-        mov eax, 1
+        mov eax, SYS_EXIT
         xor ebx, ebx
         int 0x80
