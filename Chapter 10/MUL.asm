562c731f891f6a200b231036c2b89e92f77ca694
C0uchpotato <rjacksonhelmer@gmail.com>
2022-06-10 20:17:20 -0400

    Chapter 10 Material

---
+++ Chapter 10/MUL.asm
@@ -0,0 +1,30 @@
+section .text   
+        global main
+main:
+        mov al, '3'
+        sub al, '0'
+        mov bl, '2'
+        sub bl, '0'
+        mul bl
+        add al, '0'
+        mov [res], al
+        mov ecx, msg
+        mov edx, len
+        mov ebx, 1      ;stdout
+        mov eax, 4      ;sys_write
+        int 0x80
+
+        mov ecx,res
+        mov edx, 1
+        mov ebx, 1      ;stdout
+        mov eax, 4      ;sys_write
+        int 0x80
+        mov eax, 1
+        int 0x80
+
+section .data
+msg db "The result is: ", 0xA, 0xD
+len equ $- msg
+
+segment .bss
+res resb 1
